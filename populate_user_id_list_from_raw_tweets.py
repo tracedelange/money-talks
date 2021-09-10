5f52b34465eeac9a80164738cf0a0047ffbdde0e
Trace DeLange <tracedelange@me.com>
2021-09-09 19:47:43 -0500

    adding user_id_list.csv

---
+++ populate_user_id_list_from_raw_tweets.py
@@ -0,0 +1,22 @@
+import os
+import pandas as pd
+
+
+def populate_csv(csv_file='user_id_list.csv'):
+	
+	
+
+	print("Populating")
+
+
+	return
+
+
+
+if __name__ == ("__main__"):
+	choice = input("Would you like to generate user id list based on raw tweets? (y/n): ")
+
+	if choice == "y":
+		populate_csv()
+	else:
+		print("Exiting")
