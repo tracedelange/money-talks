2a51d0053b71e3265373b2111c9df50d3802ec63
Trace DeLange <tracedelange@me.com>
2021-09-10 13:25:02 -0500

    Saving progress

--- populate_user_id_list_from_raw_tweets.py
+++ populate_user_id_list_from_raw_tweets.py
@@ -4,12 +4,35 @@ import pandas as pd
 
 def populate_csv(csv_file='user_id_list.csv'):
 	
+	#import user_id_list.csv and start iterating over each entry
 	
-
-	print("Populating")
+	users = pd.read_csv(csv_file)
+	
+		
+	print(users.head())
+	
+	for index, row, in users.iterrows():
+		print(row['user_id'])
+		find_user_in_raw_tweets(row['user_id'])
 
 
 	return
+	
+def find_user_in_raw_tweets(user_id):
+	
+	print("Populating")
+	
+	raw_tweet_dir = r'/home/pi/Code/money-talks/twitter-scraper/raw_tweets'
+	
+	dateDirs = os.listdir(raw_tweet_dir)
+	
+	dateDirs.sort()
+	dateDirs.reverse()
+	
+	print(dateDirs)	
+	
+	
+	return 
 
 
 
